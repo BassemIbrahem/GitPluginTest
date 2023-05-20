88b89f3dcdf748397abf7d55a8e9478dccfd2202
BassemIbrahem <BassemIbrahem@promise-PC>
2023-05-20 12:00:10 +0200

    first branch for me

--- src/main/resources/main/pack/JustClass.java
+++ src/main/resources/main/pack/JustClass.java
@@ -13,7 +13,7 @@ public class JustClass {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		
+		System.out.println("Hellow from the branch bug#1234 and the first git repository");
 
 	}
 
