ad6da30529bfc65382592a528ac68adbac6065bc
BassemIbrahem <BassemIbrahem@promise-PC>
2023-05-20 11:38:24 +0200

    we just trying, this the first time for me

---
+++ src/main/resources/main/pack/JustClass.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package main.pack;
+
+/**
+ * @author BassemIbrahem
+ *
+ */
+public class JustClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
